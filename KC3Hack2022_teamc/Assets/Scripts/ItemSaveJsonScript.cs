6f2553f16ace4c7152d9401a39093331e69d30b8
NemuNemu-zzz <80695330+NemuNemu-zzz@users.noreply.github.com>
2022-03-24 13:12:35 +0900

    統合作業中（セーブ回り）

--- KC3Hack2022_teamc/Assets/Scripts/ItemSaveJsonScript.cs
+++ KC3Hack2022_teamc/Assets/Scripts/ItemSaveJsonScript.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+
+[Serializable]
 public class ItemData
 {
     public string ItemName;
---
+++ KC3Hack2022_teamc/Assets/Scripts/SaveJson.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class ItemDataList{
+    public List<ItemData> itemDatas;
+    public ItemDataList(){
+        itemDatas = new List<ItemData>();
+    }
+}
+
+public class SaveJson : MonoBehaviour
+{
+    string filePath;
+    ItemDataList save;
+
+
+    void Awake()
+    {
+        //変数の初期化処理を行う
+        filePath = Application.persistentDataPath + "/" + ".savedata.json"; 
+        save = new ItemDataList();
+        ItemData testitem = new ItemData();
+        testitem.ItemName = "いちご";
+        testitem.ItemV = 3;
+        testitem.ItemY = 2022;
+        testitem.ItemM = 3;
+        testitem.ItemD = 22;
+        testitem.ItemND = 5;
+        Save(testitem);
+        Load();
+        Save(testitem);
+    }
+
+    public void Save(ItemData item)
+    {
+
+        save.itemDatas.Add(item);
+
+        Debug.Log(filePath);
+        string json = JsonUtility.ToJson(save);
+        StreamWriter streamWriter = new StreamWriter(filePath);
+        streamWriter.Write(json); streamWriter.Flush();
+        streamWriter.Close();
+        
+    }
+
+    public ItemDataList Load()
+    { 
+        if (File.Exists(filePath))
+        {
+            StreamReader streamReader;
+            streamReader = new StreamReader(filePath);
+            string data = streamReader.ReadToEnd();
+            streamReader.Close();
+            save = JsonUtility.FromJson<ItemDataList>(data);
+            Debug.Log(save.itemDatas);
+            return save;
+        }
+        return null;
+    }
+
+}
